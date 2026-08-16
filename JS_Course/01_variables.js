88168381f7518ea5a7e917739f41bb087e2b1777
nitish-1209 <113280043+nitish-1209@users.noreply.github.com>
2026-08-16 13:45:51 +0000

    Variable completed

--- JS_Course/01_variables.js
+++ JS_Course/01_variables.js
@@ -1,1 +1,8 @@
-console.log("Nitish")
+const a=1234; //cannot be modified or redeclared in the same scope
+var b="ram";//can be redeclared in the same scope
+let c ="agra";//cannot be redeclared in the same scope
+let d;//undefined
+
+console.table({a,b,c,d});
+
+// Prefer not to use var due to its scope issues and hoisting behavior. Use const for values that should not change and let for variables that may change.
