996edd2a228359a9433cca016a03901c16f6ede7
nitish-1209 <113280043+nitish-1209@users.noreply.github.com>
2026-08-16 15:36:53 +0000

    Add type conversion examples

---
+++ JS_COURSE/Basics/03_conversions .js
@@ -0,0 +1,48 @@
+/* let score=33
+
+console.log(typeof(score))
+
+ let stringScore=String(score) // String conversion
+
+console.log(stringScore);
+console.log(typeof(stringScore))
+
+let a=null;
+
+console.log(typeof(a))
+
+let stringA=String(a)
+console.log(stringA)
+console.log(typeof(stringA))
+
+// some boolean conversions :
+// 1 -> true
+// 0 -> false
+//" " -> false
+// "abc" -> true
+
+*/
+
+/* let str="123abc"
+
+console.log(typeof(str))
+
+let stringstr=Number(str)
+console.log(stringstr)
+console.log(typeof(stringstr)) */
+
+/* let x=null;
+console.log(typeof(x))
+
+let numberX=Number(x)
+console.log(numberX)
+console.log(typeof(numberX)) */
+
+
+let y=undefined;
+console.log(typeof(y))
+
+let numberY=Number(y)
+console.log(numberY)
+console.log(typeof(numberY))
+
