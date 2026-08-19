840d61bc4ef01e13148f7b75e7039d62fe2cc56d
nitish-1209 <113280043+nitish-1209@users.noreply.github.com>
2026-08-19 11:14:54 +0000

    Operators done

---
+++ JS_COURSE/Basics/04_operators.js
@@ -0,0 +1,29 @@
+/*
+console.log(2+2); //add two numbers 
+console.log(2-2); //substarct two numbers
+console.log(2*2); // multiply two numbs.
+console.log(2**3); //2 to the power 3
+console.log(2/3); //divison 
+console.log(2%3); //remainder
+ */
+
+/*
+console.log("2" + 1);
+console.log(1+"2");
+console.log("2"+1+2);
+console.log(1+2+"3");
+*/
+
+
+/*
+console.log(+true);
+//console.log(true+); //Error
+console.log(+" ");
+*/
+
+
+let x=2;
+console.log(x++);//Post-increment (x++):value is used before it is incremented.
+console.log(++x);//Pre-increment (++x):value is incremented before it is used.
+
+
